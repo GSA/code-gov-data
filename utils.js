93640e698d9d9e4c81d26ad1eb8acbb061110af5
hendersonanalytics <everett@hendersonanalytics.com>
2019-05-29 13:06:44 -0500

    only include open source and govt wide reuse projects in agency, language and license filter json creation functions; remove unused param from getUsageTypes function

--- utils.js
+++ utils.js
@@ -23,20 +23,31 @@ const sortByName = (a, b) => {
   return a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1;
 }
 
-function getUsageTypes(items) {
+function getUsageTypes() {
   return [
     { name: "Open Source", value: 'openSource' },
     { name: "Government-Wide Reuse", value: 'governmentWideReuse' }
   ]
 }
 
+function usageTypeFilter(items) {
+  const usageTypes = getUsageTypes().map((usageType) => {
+    return usageType.value;
+  });
+
+  return items.filter((item) => {
+    return item.permissions && usageTypes.includes(item.permissions.usageType)
+  });
+}
+
 function getAgencies(items) {
   const agencies = new Set();
 
-  items.forEach(item => {
+  usageTypeFilter(items).forEach(item => {
     const { acronym, name } = item.agency;
     if (acronym && name) {
       agencies.add(JSON.stringify({ 'name': name, 'value': acronym }));
+
     }
   });
 
@@ -46,7 +57,7 @@ function getAgencies(items) {
 function getLanguages(items) {
   const languages = new Set();
 
-  items.forEach(item => {
+  usageTypeFilter(items).forEach(item => {
     if (item.languages) {
       item.languages.forEach(language => {
         if (language) {
@@ -63,11 +74,10 @@ function getLanguages(items) {
   return Array.from(languages).map(JSON.parse).sort(sortByName);
 }
 
-
 function getLicenses(repos) {
   const licenses = new Set();
 
-  repos.forEach(repo => {
+  usageTypeFilter(repos).forEach(repo => {
     if (repo.permissions && repo.permissions.licenses) {
         if (Array.isArray(repo.permissions.licenses)) {
           repo.permissions.licenses.forEach(license => {
