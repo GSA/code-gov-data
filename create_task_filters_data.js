0c1dca70996f38526ab06dc3770711c67b8581d1
hendersonanalytics <everett@hendersonanalytics.com>
2019-05-29 14:03:59 -0500

    edit function invocations

--- create_task_filters_data.js
+++ create_task_filters_data.js
@@ -38,11 +38,11 @@ async function generate() {
   const tasks = await getTasks();
   console.log("tasks:", tasks.toString().substring(0, 500) + '...');
 
-  filters.agencies = getAgencies(tasks);
+  filters.agencies = getAgencies(tasks, true);
   console.log("filters.agencies:", filters.agencies);
   filters.categories = getCategories(tasks);
   console.log("filters.categories:", filters.categories);
-  filters.languages = getLanguages(tasks);
+  filters.languages = getLanguages(tasks, true);
   console.log("filters.languages:", filters.languages);
   filters.skillLevels = [
     { name: "Beginner", value: "beginner" },
